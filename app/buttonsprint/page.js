2c58d5fc59f3c17265c0e469c218d3db2ee58ad9
YY T <changiyy07@gmail.com>
2024-07-25 16:04:03 +0800

    Styled Home page with basic font size, link style, margins

--- app/buttonsprint/page.js
+++ app/buttonsprint/page.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function Page() {
   return (
-    <div>
-      <h1>Button Sprint</h1>
+    <div className="m-10">
+      <h1 className="text-4xl mb-5">Button Sprint</h1>
+      <p className="text-xl"><Link href="/" className="text-blue-800 hover:text-blue-300">Back to Home</Link></p>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Project: Tailwind Practice</h1>
-      <p>In this project, I will create simple UI components on separate pages using Tailwind CSS</p>
-      <ul>
-        <li><Link href="/buttonsprint">Button Sprint</Link></li>
-        <li><Link href="/cardsprint">Card Sprint</Link></li>
-        <li><Link href="/landingpagesprint">Landing Page Sprint</Link></li>
+    <div className="m-10">
+      <h1 className="text-4xl mb-5">Project: Tailwind Practice</h1>
+      <p className="text-xl mb-5">In this project, I will create simple UI components on separate pages using Tailwind CSS</p>
+      <ul className="list-disc list-inside">
+        <li className="text-xl"><Link href="/buttonsprint" className="text-blue-800 hover:text-blue-300">Button Sprint</Link></li>
+        <li className="text-xl"><Link href="/cardsprint" className="text-blue-800 hover:text-blue-300">Card Sprint</Link></li>
+        <li className="text-xl"><Link href="/landingpagesprint" className="text-blue-800 hover:text-blue-300">Landing Page Sprint</Link></li>
       </ul>
     </div>
   );
