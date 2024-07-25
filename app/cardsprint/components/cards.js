9c246e9fea7670ee8d5f90a58a6e5ad2ff0cd0e1
YY T <changiyy07@gmail.com>
2024-07-25 17:34:50 +0800

    Completed food card

---
+++ app/cardsprint/components/cards.js
@@ -0,0 +1,30 @@
+export function FoodCard() {
+  const backgroundImage = {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://images.unsplash.com/photo-1495214783159-3503fd1b572d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80")`
+  }
+
+  return (
+    <div
+      style={backgroundImage}
+      className="h-44 w-2/3 bg-cover text-white font-semibold text-3xl rounded-md flex justify-center items-center"
+    >
+      Breakfast
+    </div>
+  )
+}
+
+export function ProductCard() {
+  return (
+    <div>
+      ProductCard
+    </div>
+  )
+}
+
+export function TripCard() {
+  return (
+    <div>
+      TripCard
+    </div>
+  )
+}
--- app/cardsprint/page.js
+++ app/cardsprint/page.js
@@ -1,10 +1,17 @@
 import Link from 'next/link'
+import { FoodCard, ProductCard, TripCard } from './components/cards'
 
 export default function Page() {
   return (
     <div className="m-10">
       <h1 className="text-4xl mb-5">Card Sprint</h1>
       <p className="text-xl"><Link href="/" className="text-blue-800 hover:text-blue-300">Back to Home</Link></p>
+      <p className="text-l my-4">Food Card</p>
+      <FoodCard />
+      <p className="text-l my-4">Product Card</p>
+      <ProductCard />
+      <p className="text-l my-4">Trip Card</p>
+      <TripCard />
     </div>
   )
 }
