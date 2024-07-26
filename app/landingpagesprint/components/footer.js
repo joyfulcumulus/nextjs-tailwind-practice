b03e7ad028307c8fd31594d1bea5331d98d48d81
YY T <changiyy07@gmail.com>
2024-07-26 18:30:51 +0800

    import fontawesome css into root layout to fix huge icon issue as reported in their website

--- app/landingpagesprint/components/footer.js
+++ app/landingpagesprint/components/footer.js
@@ -2,13 +2,21 @@ import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
-
 export function Footer() {
   return (
     <div className="text-center p-16 bg-stone-900">
       <div>
-        <Link href="#" className="">
-          <span>icon</span>
+        <Link href="#" className="text-green-400 px-3">
+          Test
+        </Link>
+        <Link href="#" className="text-green-400 px-3">
+          Test
+        </Link>
+        <Link href="#" className="text-green-400 px-3">
+          Test
+        </Link>
+        <Link href="#" className="text-green-400 px-3">
+          Test
         </Link>
         <p className="text-stone-400 text-base">This website was coded with <FontAwesomeIcon icon={faHeart} /> at Le Wagon.</p>
       </div>
