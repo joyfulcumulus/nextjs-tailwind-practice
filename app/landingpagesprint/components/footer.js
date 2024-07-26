24d2ec5349a0cc94bb436d8e9792455698754f1d
YY T <changiyy07@gmail.com>
2024-07-26 18:40:08 +0800

    Complete styling of footer

--- app/landingpagesprint/components/footer.js
+++ app/landingpagesprint/components/footer.js
@@ -1,24 +1,25 @@
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faGithub, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 export function Footer() {
   return (
     <div className="text-center p-16 bg-stone-900">
       <div>
-        <Link href="#" className="text-green-400 px-3">
-          Test
+        <Link href="#" className="text-green-400 px-3 text-4xl">
+          <FontAwesomeIcon icon={faFacebook} />
         </Link>
-        <Link href="#" className="text-green-400 px-3">
-          Test
+        <Link href="#" className="text-green-400 px-3 text-4xl">
+          <FontAwesomeIcon icon={faGithub} />
         </Link>
-        <Link href="#" className="text-green-400 px-3">
-          Test
+        <Link href="#" className="text-green-400 px-3 text-4xl">
+          <FontAwesomeIcon icon={faTwitter} />
         </Link>
-        <Link href="#" className="text-green-400 px-3">
-          Test
+        <Link href="#" className="text-green-400 px-3 text-4xl">
+          <FontAwesomeIcon icon={faInstagram} />
         </Link>
-        <p className="text-stone-400 text-base">This website was coded with <FontAwesomeIcon icon={faHeart} /> at Le Wagon.</p>
+        <p className="text-stone-400 text-base my-3">This website was coded with <FontAwesomeIcon icon={faHeart} /> at Le Wagon.</p>
       </div>
     </div>
   )
