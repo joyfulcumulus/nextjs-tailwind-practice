22a684236dd3eee546b68e539b7494d6359049ef
YY T <changiyy07@gmail.com>
2024-07-30 22:27:10 +0800

    Styled product card (except stars for rating)

--- app/opencomponent/components/card.js
+++ app/opencomponent/components/card.js
@@ -1,9 +1,11 @@
 import Link from "next/link"
 import Image from "next/image"
 
-export default function Card() {
+export default function Card({ title="Item title", desc="A product description", rating=3, price=65}) {
+  const formattedRating = rating.toFixed(1);
+
   return (
-    <div className="bg-white p-4">
+    <div className="bg-white p-4 shadow">
       <div className="h-32 relative">
         <Image
           src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -13,19 +15,19 @@ export default function Card() {
         />
       </div>
 
-      <div className="flex justify-between">
+      <div className="flex justify-between pt-5 pb-7">
         <div>
-          <p>title</p>
-          <p>desc</p>
-          <p>rating</p>
+          <p className="text-base font-semibold text-slate-800">{title}</p>
+          <p className="text-xs text-gray-400 my-1">{desc}</p>
+          <p className="text-xs text-gray-400">({formattedRating})</p>
         </div>
-        <div>
-          $price
+        <div className="text-base font-semibold text-blue-400">
+          ${price}
         </div>
       </div>
 
-      <div className="text-center">
-        <Link href="#">Add to Cart</Link>
+      <div className="text-center text-xs font-semibold text-gray-400 border-t border-slate-200 py-3">
+        <Link href="#" className="inline-block">Add to Cart</Link>
       </div>
     </div>
   )
--- app/opencomponent/page.js
+++ app/opencomponent/page.js
@@ -7,7 +7,7 @@ export const metadata = {
 export default function Page() {
   return (
     <div className="bg-gray-100 px-4">
-      <h1 className="text-2xl py-4 font-bold">Products</h1>
+      <h1 className="text-2xl py-4 font-bold text-slate-800">Products</h1>
 
       <div className="grid grid-cols-4 gap-4">
         <Card />
