418971622d637f3c2204117fbfad2963411503ed
YY T <changiyy07@gmail.com>
2024-07-31 18:40:41 +0800

    Add scale 110 on hover on product card

--- app/opencomponent/components/card.js
+++ app/opencomponent/components/card.js
@@ -7,7 +7,7 @@ export default function Card({ title="Item title", desc="A product description",
   const formattedRating = rating.toFixed(1);
 
   return (
-    <div className="bg-white p-4 shadow">
+    <div className="bg-white p-4 shadow hover:scale-110 transition cursor-pointer">
       <div className="h-32 relative">
         <Image
           src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
